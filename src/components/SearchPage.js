f9b9348af57f813dedc5c5e001f090534a7b364a
Pawan Saroj <pawansaroj6783@gmail.com>
2020-11-21 18:30:58 +0530

    airbnb clone

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,17 +1,16 @@
-import { Button } from '@material-ui/core';
 import React from 'react';
 import '../stylesheets/SearchPage.css';
-import SearchResult from './SearchResult';
+import { Button } from "@material-ui/core";
+import SearchResult from "./SearchResult";
 
 function SearchPage() {
     return (
-        <div className="searchPage">
-            <div
-            className="searchPage_info">
-                <p>62 stays . 26 august to 30 august . 2 guest</p>
+        <div className='searchPage'>
+            <div className='searchPage__info'>
+                <p>62 stays · 26 august to 30 august · 2 guest</p>
                 <h1>Stays nearby</h1>
                 <Button variant="outlined">Cancellation Flexibility</Button>
-                <Button variant="outlined">Types of place</Button>
+                <Button variant="outlined">Type of place</Button>
                 <Button variant="outlined">Price</Button>
                 <Button variant="outlined">Rooms and beds</Button>
                 <Button variant="outlined">More filters</Button>
@@ -80,8 +79,8 @@ function SearchPage() {
                 star={3.85}
                 price="£90 / night"
                 total="£650 total"
-            />       
-            </div>
+            />
+        </div>
     )
 }
 
